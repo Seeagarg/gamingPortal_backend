872b9a455835f6c1436fffce78ce58e42d377ce6
Seeagarg <89650047+Seeagarg@users.noreply.github.com>
2024-04-18 15:16:46 +0530

    env set

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 //connecting backend to database
 const mysql = require('mysql');
+require('dotenv').config();
 const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'seea@020203',
-    database:'gaming_portal'
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 });
 
 connection.connect((err)=>{
